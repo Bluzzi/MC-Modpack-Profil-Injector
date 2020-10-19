03ed4d38998033532c7287af171563cb7e30c5b5
Bluzzi <bluzzigame@gmail.com>
2020-10-19 02:35:12 +0200

    Use totalmem instead of freemem

--- assets/javascripts/memory-slider.js
+++ assets/javascripts/memory-slider.js
@@ -1,7 +1,7 @@
 let slider = document.getElementById("memory");
 
 slider.min = 2000;
-slider.max = require("os").freemem / Math.pow(1024, 2);
+slider.max = require("os").totalmem / Math.pow(1024, 2);
 slider.value = Math.round(slider.max / 2);
 
 updateValueIndicator(slider);
@@ -12,5 +12,5 @@ updateValueIndicator(slider);
 function updateValueIndicator(slider){
     let text = document.getElementById("memory-text");
 
-    text.innerHTML = "Dédiée : " + slider.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " Mo"; 
+    text.innerHTML = "Dédiée : " + slider.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " Mo";
 }
